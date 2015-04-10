ea1127525ade5f2f86fac27a5319743c3ef1937f
sluggets <tim.longoria@gmail.com>
2015-04-10 11:10:03 -0500

    Front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -43,7 +43,7 @@ class NewVisitorTest(unittest.TestCase):
 
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 
         #There is still a text box inviting her to add another item.
         # She enters "use peacock feathers to make a fly"( Edith is very
