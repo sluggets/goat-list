b8d2e28b06d7644acb5b8402cc130d585f084b8e
sluggets <tim.longoria@gmail.com>
2015-04-14 11:51:38 -0500

    make FT an app and now use disposable LiveServer

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,12 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(7)
@@ -17,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new onlinen to-do app
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
 
         # She notices the page title and header mention to-do lists
@@ -69,5 +71,5 @@ class NewVisitorTest(unittest.TestCase):
 
     # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+#if __name__ == '__main__':
+    #unittest.main(warnings='ignore')
