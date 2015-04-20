6eb964b7a9572173efe69f3f354ba77feb219bf5
sluggets <tim.longoria@gmail.com>
2015-04-20 11:42:21 -0500

    first steps of FT for layout+styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -63,40 +63,49 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 
-    # The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-    # Now a new user, Francis comes along to the site
+        # Now a new user, Francis comes along to the site
 
-    # We use a new browser session to make sure that no information of
-    # Edith's is coming through from cookies etc.
+        # We use a new browser session to make sure that no information of
+        # Edith's is coming through from cookies etc.
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-    # Francis visits the home page. There is no sign of Edith's list
+        # Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
 
-    # Francis starts a new list by entering a new item. He is less
-    # interesting than Edith...
+        # Francis starts a new list by entering a new item. He is less
+        # interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-    # Francis gets his own unique URL
+        # Francis gets his own unique URL
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-    # Again, there is no trace of Edith's list
+        # Again, there is no trace of Edith's list
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-    # Satisfied, they both go back to sleep
-
-#if __name__ == '__main__':
-    #unittest.main(warnings='ignore')
+        # Satisfied, they both go back to sleep
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
