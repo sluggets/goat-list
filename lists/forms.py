6cfc6e81e14e13ac20a87ddc25212716614feb6a
sluggets <tim.longoria@gmail.com>
2015-04-28 11:51:34 -0500

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/forms.py
+++ lists/forms.py
@@ -10,4 +10,3 @@ class ItemForm(forms.models.ModelForm):
         fields = ('text',)
         widgets = {'text': forms.fields.TextInput(attrs={'placeholder': 'Enter a to-do item', 'class': 'form-control input-lg'})}
         error_messages = {'text': {'required': EMPTY_ITEM_ERROR}}
-    #item_text = forms.CharField(widget=forms.fields.TextInput(attrs={'placeholder': 'Enter a to-do item', 'class': 'form-control input-lg'}))
